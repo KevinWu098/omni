356619042e1e598fd029ef9860a67cea0dd6cbbf
Jay Wu <jayqw@uci.edu>
2025-06-21 21:14:55 -0700

    chore: clean

--- server/agent/app.py
+++ server/agent/app.py
@@ -8,16 +8,11 @@ from service import AgentService
 load_dotenv()
 
 app = Flask(__name__)
-# manager = AgentManager()
-
 
 @app.route("/run_command", methods=["POST"])
 def run_command():
     data = request.get_json(force=True)
-    # agent_id = data.get("agent_id")
     command = data.get("command")
-    # if not agent_id:
-    #     return jsonify({"error": "Missing 'agent_id' in JSON body"}), 400
     if not command:
         return jsonify({"error": "Missing 'command' in JSON body"}), 400
     service = AgentService()
