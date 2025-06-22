fd6c68b5f7647b2f49359ab896a78947ac07a130
Kevin Wu <kevinwu098@gmail.com>
2025-06-21 17:31:29 -0700

    refactor: move files around

--- client/src/components/builder/all-side-panel.tsx
+++ client/src/components/builder/all-side-panel.tsx
@@ -1,5 +1,5 @@
-import { type Test } from "@/components/ui/builder/content";
-import { TestCard } from "@/components/ui/builder/test-card";
+import { type Test } from "@/components/builder/content";
+import { TestCard } from "@/components/builder/test-card";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { ScrollArea } from "@/components/ui/scroll-area";
